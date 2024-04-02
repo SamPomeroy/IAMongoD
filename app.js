e7b8d7c87fd941e372f6a6781dd93d0a70995faf
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-04-01 21:00:59 -0500

    completed and working but with empty object

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const logger = require('morgan')
+
+
+
+const app = express()
+const productsRouter = require('./routes/product/productsRouter')
+
+
+app.use(logger('dev'))
+app.use(express.json())
+
+app.use('/api/products', productsRouter)
+
+
+
+module.exports = app
---
+++ routes/product/controller/productController.js
@@ -0,0 +1,44 @@
+const User = require('../model/Product')
+
+async function getAllProducts(){
+    try {
+        const foundAllProducts = await Product.find({})
+        return foundAllProducts
+    } catch (error) {
+        return error
+    }
+}
+
+async function createProduct(body){
+    try {
+        const addProduct = new Product({
+            productName: body.productName
+        })
+        return await addProduct.save()
+    } catch (error) {
+        return error
+    }
+}
+
+async function getProductById(id){
+    try {
+        return await Product.find({_id: id})
+    } catch (error) {
+        return error
+    }
+}
+
+async function updateProductById(id, body){
+    try {
+        return await Product.findByIdAndUpdate({_id: id}, body, {new:true})
+    } catch (error) {
+        return error
+    }
+}
+
+module.exports = {
+    getAllProducts, 
+    createProduct,
+    getProductById,
+    updateProductById
+}
